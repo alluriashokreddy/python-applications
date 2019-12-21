3fcb0ad6fbe8a1bfdabebdc3c5b14e60436c8903
Ashok <alluriashok512@gmail.com>
2019-12-22 01:32:07 +0530

    Initial Commit

---
+++ snippets/builtin-functions/built-in-functions-for-collections.py
@@ -0,0 +1,68 @@
+# zip function
+lhead = ['first','middle','last']
+lrow = ['ashok','reddy','alluri']
+
+for t in zip(lhead,lrow):
+    print(t)
+
+# output:
+# ('first', 'ashok')
+# ('middle', 'reddy')
+# ('last', 'alluri')
+
+
+# map function
+def addition(first, second):
+    return first+second
+
+
+l1 = [1, 2, 3, 4]
+l2 = [4, 5, 6, 7]
+for t in map(addition,l1, l2):
+    print(t)
+
+# output
+# 5
+# 7
+# 9
+# 11
+
+# enumerate function
+l = ['ashok', 'reddy', 'alluri']
+for i in enumerate(l):
+    print(i)
+# output
+# (0, 'ashok')
+# (1, 'reddy')
+# (2, 'alluri')
+
+
+# filter function
+def check(val):
+    if val > 10:
+        return True
+    else:
+        return False
+
+
+l = [10, 12, 1, 4, 7, 11]
+for i in filter(check, l):
+    print(i)
+
+# output
+# 12
+# 11
+
+# min, max, sum functions
+l = [10, 12, 1, 4, 7, 11]
+print(min(l))
+print(max(l))
+print(sum(l))
+print(len(l))
+print(sorted(l))  # returns the sorted list, not in-line change,
+print(l)
+
+# output
+# 1
+# 12
+# 45
