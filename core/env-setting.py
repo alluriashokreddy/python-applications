9a65a7272fea98a7ff1cb36bd8444db9d457a8fd
Ashok <alluriashok512@gmail.com>
2020-01-27 00:13:06 +0530

    different python libraries

--- core/env-setting.py
+++ core/env-setting.py
@@ -8,7 +8,6 @@ print(sys.path)
 # output
 # ['D:\\Workspace\\Python\\snippets', 'D:\\Workspace\\Python', 'C:\\Users\\Ashok\\Anaconda3\\envs\\Python\\python37.zip', 'C:\\Users\\Ashok\\Anaconda3\\envs\\Python\\DLLs', 'C:\\Users\\Ashok\\Anaconda3\\envs\\Python\\lib', 'C:\\Users\\Ashok\\Anaconda3\\envs\\Python', 'C:\\Users\\Ashok\\Anaconda3\\envs\\Python\\lib\\site-packages']
 
-
 # info: sys.path is a list, so you can add other packages for lookup just the way you add to list data structure
 
 
